0ba90d23ad99319b9b2c45e5b86c39794ebce4ed
SongkhomSitthirat <prew.sitthirat@gmail.com>
2021-03-02 15:14:01 +0700

    Add regex email.

--- app/src/main/java/com/dtac/mobile/component/MainActivity.kt
+++ app/src/main/java/com/dtac/mobile/component/MainActivity.kt
@@ -2,18 +2,16 @@ package com.dtac.mobile.component
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
-import kotlinx.android.synthetic.main.activity_main.*
-import th.co.dtac.library.DtacProgressDialog
-import th.co.dtac.library.textview.TextViewSeeMore
+import android.util.Log
+import th.co.dtac.library.textfield.DtacTextField
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val tvSeeMore = findViewById<TextViewSeeMore>(R.id.tvSeeMore)
-        tvSeeMore.setTextSeeMore("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+        findViewById<DtacTextField>(R.id.edtTest).addTextConditionsChangeListener {
+            Log.d("Prew", "Text Change: $it")
+        }
     }
 }
--- dtac-component/src/main/java/th/co/dtac/library/textfield/DtacTextField.kt
+++ dtac-component/src/main/java/th/co/dtac/library/textfield/DtacTextField.kt
@@ -1,14 +1,17 @@
 package th.co.dtac.library.textfield
 
 import android.content.Context
+import android.graphics.Typeface
 import android.text.Editable
 import android.text.InputFilter
 import android.text.InputFilter.LengthFilter
 import android.text.InputType
 import android.text.TextWatcher
 import android.util.AttributeSet
-import android.util.Log
 import androidx.appcompat.widget.AppCompatEditText
+import androidx.core.content.res.ResourcesCompat
+import th.co.dtac.library.DtacWidget
+import th.co.dtac.library.R
 
 
 /**
@@ -18,31 +21,59 @@ import androidx.appcompat.widget.AppCompatEditText
 class DtacTextField : AppCompatEditText {
 
     private var listener: ((Boolean) -> Unit)? = null
+    private var regexType = 0
+    private var regex = ""
+
+    companion object {
+        private const val REGEX_MSISDN_TYPE = 1
+        private const val REGEX_MSISDN = "[6]{2}+\\d{9}"
+        private const val REGEX_PHONE_NUM = "[0][\\d]{9}"
+        private const val REGEX_EMAIL_TYPE = 2
+        private const val REGEX_EMAIL = "[\\w\\.\\+]{1,60}\\@[a-zA-Z0-9][a-zA-Z0-9\\-]{0,64}(\\.[a-zA-Z0-9][a-zA-Z0-9\\-]{0,25})+"
+    }
 
     constructor(context: Context) : super(context) {
         initAttr(null)
+        setFont()
         initProperties()
     }
 
     constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
         initAttr(attrs)
+        setFont()
         initProperties()
     }
 
     constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
         initAttr(attrs)
+        setFont()
         initProperties()
     }
 
     private fun initAttr(attrs: AttributeSet?) {
         attrs?.let {
-
+            val typedArray = context.obtainStyledAttributes(attrs, R.styleable.DtacTextField)
+            regexType = typedArray.getInt(R.styleable.DtacTextField_regexType, 0)
+            typedArray.recycle()
         }
     }
 
     private fun initProperties() {
-        filters = arrayOf<InputFilter>(LengthFilter(11))
-        inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_SIGNED
+        when (regexType) {
+            REGEX_MSISDN_TYPE -> {
+                filters = arrayOf<InputFilter>(LengthFilter(11))
+                inputType = InputType.TYPE_CLASS_NUMBER or InputType.TYPE_NUMBER_FLAG_SIGNED
+                regex = REGEX_MSISDN
+            }
+            REGEX_EMAIL_TYPE -> {
+                filters = arrayOf<InputFilter>(LengthFilter(50))
+                inputType = InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS
+                regex = REGEX_EMAIL
+            }
+            else -> {
+
+            }
+        }
         this.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
@@ -51,40 +82,33 @@ class DtacTextField : AppCompatEditText {
             }
 
             override fun afterTextChanged(s: Editable?) {
-                Log.d(this@DtacTextField.javaClass.simpleName, getMSISDN())
-                isMSISDN(s)
-                listener?.invoke(isMSISDN(s))
+                if (s.isNullOrBlank()) {
+                    listener?.invoke(false)
+                } else {
+                    if (regexType == REGEX_MSISDN_TYPE) {
+                        listener?.invoke(REGEX_MSISDN.toRegex().matches(s) || REGEX_PHONE_NUM.toRegex().matches(s))
+                    } else {
+                        listener?.invoke(regex.toRegex().matches(s))
+                    }
+                }
             }
         })
     }
 
-    fun addTextConditionsChangeListener(listener: (Boolean) -> Unit) {
-        this.listener = listener
-    }
-
-    private fun isMSISDN(text: Editable?): Boolean {
-        return if (text.isNullOrBlank()) {
-            false
-        } else {
-            when {
-                text.startsWith("0") -> {
-                    text.length == 10
-                }
-                text.startsWith("66") -> {
-                    if ("\\d{11}".toRegex().matches(text)) {
-                        Log.d("Prew", "66 regex true")
-                        true
-                    } else {
-                        Log.d("Prew", "66 regex false")
-                        false
-                    }
-
-                }
-                else -> {
-                    false
-                }
+    private fun setFont() {
+        val font: Typeface? = when (DtacWidget.currentLanguage().toUpperCase()) {
+            "MM", "MY" -> {
+                ResourcesCompat.getFont(context, R.font.myanmar_sagar)
+            }
+            else -> {
+                ResourcesCompat.getFont(context, R.font.dtac2018_regular)
             }
         }
+        typeface = font ?: ResourcesCompat.getFont(context, R.font.dtac2018_regular)
+    }
+
+    fun addTextConditionsChangeListener(listener: (Boolean) -> Unit) {
+        this.listener = listener
     }
 
     fun getMSISDN(): String {
